5aae8dad2e249cf0363f5380fe2ff996b1916a9f
Viet Hua <huaviet999@gmail.com>
2022-08-20 11:09:14 +0700

    update ci script

--- Scripts/github-actions/run_unit_tests.sh
+++ Scripts/github-actions/run_unit_tests.sh
@@ -1,20 +1,8 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2513
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+#!/bin/bash
+set -e
 
-\f0\fs24 \cf0 #!/bin/bash\
-set -e\
-\
-scheme=$1\
-\
-echo \'93Destination: $scheme\'94\
-\
-\
-echo \'93Build successful\'94\
-\
-\
-\
-}
+scheme=$1
+
+echo “Destination: $scheme
+
+echo “Build successful”
